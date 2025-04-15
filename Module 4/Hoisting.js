cef6a0b4905013bfea4fceee646546fb934a2cec
yasvanthasai <72198764+yasvanthasai@users.noreply.github.com>
2025-04-15 00:13:29 -0400

    Hoisting concept

--- Module 4/Hoisting.js
+++ Module 4/Hoisting.js
@@ -1,8 +1,21 @@
+//in memory excution a will be undefined
+// and function would be stored as greet initially
+//this above senario comes when we execute line 13 and 14 first before the code
+
+//add(2,4) - this won't work
+
 var a = 'hi'
 function greet(){
     console.log('hello from function')
 }
-
+//above is normal function
 
 console.log(a)
 greet()
+
+//below is function expression
+var add = function(a,b){
+    console.log(a+b)
+}
+
+//add(2,4) - this will work as we defined the var above
