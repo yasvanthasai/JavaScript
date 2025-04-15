966615fa9dad09b404dce38ae1eb05087f48065d
yasvanthasai <72198764+yasvanthasai@users.noreply.github.com>
2025-04-15 11:48:51 -0400

    exporting code in js

---
+++ Module 5/main.js
@@ -0,0 +1,10 @@
+const calculator = require('./calci')
+
+calculator.addition(2,3)
+
+// these 3 will also work in the same way as addition
+// calculator.division
+// calculator.multiplication
+// calculator.substraction
+
+
