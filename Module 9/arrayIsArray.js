18ebc65ed66452eb7349146febe780564755c551
yasvanthasai <72198764+yasvanthasai@users.noreply.github.com>
2025-05-25 21:48:53 -0400

    final notes

--- Module 9/arrayIsArray.js
+++ Module 9/arrayIsArray.js
@@ -1,3 +1,12 @@
 let arr = [1,2,3]
 Array.isArray(arr) // true
 Array.isArray({}) // false
+
+function Person(firstName, lastName) {
+this.firstName = firstName;
+this.lastName = lastName;
+}
+const lydia = new Person('Lydia', 'Hallie');
+const sarah = Person('Sarah', 'Smith');
+console.log(lydia);
+console.log(sarah);
